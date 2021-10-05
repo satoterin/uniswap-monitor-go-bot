b6668ef8bd0db6c767bd0dce1aff469177172ee3
satoterin <judekinsley911@gmail.com>
2021-10-05 12:20:44 +0530

    ft: parse Swap events

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"context"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/joho/godotenv"
+	"github.com/pradeep-selva/uniswap-monitor/UniswapUSDC2Pool"
 )
 
 func GetEnvVar(key string) string {
@@ -42,13 +45,30 @@ func main() {
 	checkError(err)
 
 	log.Println("Listening for Uniswap USDC2 pool's events...")
-	
+
+	uniswapAbi, err := abi.JSON(strings.NewReader(string(UniswapUSDC2Pool.UniswapUSDC2PoolABI)))
+	checkError(err)
+
 	for {
 		select {
-		case err := <-sub.Err():
-			log.Fatal(err)
-		case event := <-logs:
-			log.Println(event)
+			case err := <-sub.Err():
+				log.Fatal(err)
+			case vLog := <-logs:
+				// event := struct {
+				// 	Sender [32]byte
+				// 	Recipient [32]byte
+				// 	Amount0 [32]byte
+				// 	Amount1 [32]byte
+				// 	SqrtPriceX96 [32]byte
+				// 	Liquidity [32]byte
+				// 	Tick [32]byte
+				// }{}
+
+				log.Println("--------- NEW SWAP ---------")
+				swapEvent, err := uniswapAbi.Unpack("Swap", vLog.Data)
+				checkError(err)
+
+				log.Println(swapEvent...)
 		}
 	}
 }
